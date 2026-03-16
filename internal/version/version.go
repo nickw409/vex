3a1541c7f5c49927b2ff9c519f7673ff4adf0665
nickw409 <nwiley333@gmail.com>
2026-03-16 16:35:12 -0700

    fix: resolve version info for go install users
    Use runtime/debug.ReadBuildInfo as fallback when ldflags aren't set,
    so go install picks up the module version and VCS metadata.

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,14 +3,53 @@ package version
 import (
 	"fmt"
 	"runtime"
+	"runtime/debug"
 )
 
 // Set via ldflags at build time.
 var (
-	Version = "dev"
-	Commit  = "unknown"
-	Date    = "unknown"
+	Version = ""
+	Commit  = ""
+	Date    = ""
 )
+
+func init() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+
+	if Version == "" && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		Version = info.Main.Version
+	}
+
+	if Commit == "" {
+		for _, s := range info.Settings {
+			switch s.Key {
+			case "vcs.revision":
+				if len(s.Value) > 7 {
+					Commit = s.Value[:7]
+				} else {
+					Commit = s.Value
+				}
+			case "vcs.time":
+				if Date == "" {
+					Date = s.Value
+				}
+			}
+		}
+	}
+
+	if Version == "" {
+		Version = "dev"
+	}
+	if Commit == "" {
+		Commit = "unknown"
+	}
+	if Date == "" {
+		Date = "unknown"
+	}
+}
 
 func String() string {
 	return fmt.Sprintf("vex %s (%s) built %s %s/%s", Version, Commit, Date, runtime.GOOS, runtime.GOARCH)

ce008bc1d2d70d1df0290e10aa0bc589f75d30d1
nickw409 <nwiley333@gmail.com>
2026-03-16 17:16:46 -0700

    fix: create .vex directory during init

--- internal/cli/init.go
+++ internal/cli/init.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/nickw409/vex/internal/config"
 	"github.com/spf13/cobra"
@@ -12,10 +13,13 @@ func newInitCmd() *cobra.Command {
 		Use:   "init",
 		Short: "Create a default vex.yaml config file",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := os.MkdirAll(".vex", 0o755); err != nil {
+				return fmt.Errorf("creating .vex directory: %w", err)
+			}
 			if err := config.WriteDefault("vex.yaml"); err != nil {
 				return err
 			}
-			fmt.Fprintln(cmd.ErrOrStderr(), "Created vex.yaml")
+			fmt.Fprintln(cmd.ErrOrStderr(), "Created vex.yaml and .vex/")
 			return nil
 		},
 	}
